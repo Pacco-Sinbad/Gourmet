aa6479decf09cd2dff89daca501ad6010b230a9e
Mathias Fischer <mfischerf92@gmail.com>
2024-09-10 21:36:09 -0500

    adding a server option to the website to allow for log ins in the future.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const PORT = process.env.PORT || 10000
+const cors = require('cors');
+const e = require('express');
+app.use (cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const dotenv = require('dotenv').config()
+const uri = process.env.MONGOLAB_URI
+
+MongoClient.connect(uri)
+    .then(client => {
+        console.log('connected')
+        const userListDB = client.db('UserList')
+
+        app.get('/', (req,res)=> {
+            res.sendFile(__dirname + '/index.html')
+        })
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+
+
+
+
+    })
+    .catch(error => console.error(error))
