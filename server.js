77c87f045ea1492c267212077ff34e8d9f03cb30
Mathias Fischer <mfischerf92@gmail.com>
2024-09-12 22:57:35 -0500

    a;lsdkfj

--- server.js
+++ server.js
@@ -9,18 +9,36 @@ app.use (cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 const dotenv = require('dotenv').config()
 const uri = process.env.MONGOLAB_URI
 
-// MongoClient.connect(uri)
-//     .then(client => {
-//         console.log('connected')
-//         const ########## = client.db('UserList')
+MongoClient.connect(uri)
+    .then(client => {
+        console.log('connected')
+        const shroomTrack = client.db('Shroomed')
 
         app.get('/', (req,res)=> {
-            res.sendFile(__dirname + '/index.html')
+            res.render('index')
         })
+
+        app.get('/home', (req,res)=> {
+            res.render('index')
+        })
+
+        app.get('/about', (req,res)=> {
+            res.render('about')
+        })
+
+        app.get('/care', (req,res)=> {
+            res.render('FAQ')
+        })
+
+        app.get('/album', (req,res)=> {
+            res.render('lookBook')
+        })
+////////////////////////////////////////////////////////////////////////////////////////////////////////////        
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`)
         })
@@ -28,5 +46,5 @@ const uri = process.env.MONGOLAB_URI
 
 
 
-    // })
-    // .catch(error => console.error(error))
+    })
+    .catch(error => console.error(error))
